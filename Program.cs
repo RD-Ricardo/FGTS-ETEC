8226d9ad7beff2722732207d7249a0706c0ff5b5
RD-Ricardo <ricardodias169@gmail.com>
2021-03-19 21:28:22 -0300

    Finalizando Programa

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace FGTS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double num1, resp;
+
+            Console.WriteLine("--------");
+            Console.WriteLine("--FGTS--");
+            Console.WriteLine("--------");
+            Console.Write("Digite seu salário(R$): ");
+            num1 = Convert.ToDouble(Console.ReadLine());
+            resp = num1 * 8 / 100;
+            Console.Write($"FGTS: {resp.ToString("c")}");
+
+        }
+    }
+}
